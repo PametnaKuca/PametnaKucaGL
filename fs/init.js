138a44299d880d7215537e420794eb9564e079a6
Adam Sedmak <adam.sedmak@fer.hr>
2017-09-20 17:33:22 +0200

    Data from STM32 sent to Azure and then Webapp

--- fs/init.js
+++ fs/init.js
@@ -1,25 +1,77 @@
-//load('api_config.js');
+load('api_config.js');
 //load('api_dht.js');
-//load('api_mqtt.js');
+load('api_mqtt.js');
 load('api_sys.js');
 load('api_timer.js');
 load('api_uart.js');
+load('api_net.js');
 
 let uartNo = 0;		//uart number
-let rxAcc = "";		//accumulaterd Rx data, will be echoed back to Tx
-let value = false;
-//let topic = 'devices/' + Cfg.get('device.id') + '/messages/events/';
-//let RxPin = 13;
-//let TxPin = 15;
+let isConnected = false;
+let isMQTTConnected = false;
 
+let deviceId = Cfg.get('device.id');
+let topic = 'devices/' + deviceId + '/messages/events/';
+
+/* Imported C functions */
+let parseTemperature = ffi('char* returnTemperature(char*)');
+let parseHumidity = ffi('char* returnHumidity(char*)');
 
 // Configure UART at 115200 baud
 UART.setConfig(uartNo, {
 	baudRate: 115200,
-	esp8266: {
-		swapRxCtsTxRts: true,	//GPIO15 becomes TX instead of GPIO1, GPIO13 RX
-	},
+//	esp8266: {
+//		swapRxCtsTxRts: true,	//GPIO15 becomes TX instead of GPIO1, GPIO13 RX
+//	},
 });
+
+
+
+/* ESP module meta info */
+let getInfo = function() {
+	return JSON.stringify({
+		"deviceId": deviceId,
+		"total_ram": Sys.total_ram(),
+		"free_ram": Sys.free_ram(),
+		"uptime": Sys.uptime(),
+		"t": Timer.now()
+	});
+};
+
+Net.setStatusEventHandler(function(ev, arg){
+	print("Wifi Event:", ev);
+	if(ev === Net.STATUS_DISCONNECTED) {
+		print("Wifi DISCONNECTED - Event time:", Timer.now());
+		isConnected = false;
+	}
+	if(ev === Net.STATUS_CONNECTING) {
+		print("Wifi CONNECTING - Event time:", Timer.now());
+		isConnected = false;
+	}
+	if(ev === Net.STATUS_CONNECTED) {
+		print("Wifi CONNECTED - Event time:", Timer.now());
+		isConnected = true;
+	}
+	if(ev === Net.STATUS_GOT_IP) {
+		print("Device got IP - Event time:", Timer.now());
+		isConnected = true;
+	}
+}, null);
+
+
+MQTT.setEventHandler(function(conn, ev, evdata){
+	if(ev === MQTT.EV_CONNACK) {
+		isMQTTConnected = true;
+		print("MQTT Connection Acknowledge:", JSON.stringify(ev));
+	} //else {
+		//isMQTTConnected = false;
+		//print("Nisam povezan");
+	//}
+	if(ev === 214) {
+		isMQTTConnected = false;
+		print("MQTT DisConnection:", JSON.stringify(ev));
+	}
+}, null);
 
 
 /*	Set Dispatcher callback that will be called whenever new Rx data or 
@@ -27,39 +79,24 @@ UART.setConfig(uartNo, {
  */
 UART.setDispatcher(uartNo, function(uartNo, ud) {
 	let ra = UART.readAvail(uartNo);
+	let oa = UART.writeAvail(uartNo);
 	if (ra > 0) {
 		// Received new data: print it immediately to the console, and also
 		// accumulate in the "rxAcc" variable which will be echoed back to UART later
-		let data = UART.read(uartNo);
-		print ('Received UART data:', data);
-		rxAcc += data;
-	}
+		let rec = UART.read(uartNo);
+		//UART.write(uartNo, 'Received UART data:' + data + '\r\n');
+		print("Received UART data:", rec);
+		let temp = parseTemperature(rec);
+		print('Parsed temperature:', temp);
+		let hum = parseHumidity(rec);
+		print('Parsed humidity:', hum);		
+		let msg = JSON.stringify({ "temperature": temp, "humidity": hum });
+		print('Poruka za slanje:', msg);
+		//print(msg);
+		let ok = MQTT.pub(topic, msg, 1);
+		print("Published: ", ok ? "yes" : "no", "Response", ok);
+	}  
 }, null);
 
 // Enable Rx
 UART.setRxEnabled(uartNo, true);
-
-
-// Send UART data every second
-Timer.set(1000 /* milliseconds */, true /* repeat */, function() {
-
-  value = !value;
-  UART.write(
-	uartNo,
-	'Hello UART! '
-		+ (value ? 'Tick' : 'Tock')
-		+ ' uptime: ' + JSON.stringify(Sys.uptime())
-		+ ' RAM: ' + JSON.stringify(Sys.free_ram())
-		+ (rxAcc.length > 0 ? (' Rx: ' + rxAcc) : '')
-		+ '\r\n'
-  );
-  rxAcc = '';
-
-  /*
-   * let msg = JSON.stringify({ temperature: dht.getTemp(), humidity: dht.getHumidity() });
-
-   * let ok = MQTT.pub(topic, msg, 1);
-   * print(ok, topic, '->', msg);
-   */
-}, null);
-
--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "common/platform.h"
 #include "common/cs_file.h"
@@ -9,6 +10,46 @@
 #include "mgos_hal.h"
 #include "mgos_dlsym.h"
 #include "mjs.h"
+
+char *returnTemperature(char *str){
+    static char temp[10] = "";
+    int i;
+    int offset = 0;
+    for(i = 0; str[i] != '\0'; i++){
+        if(((str[i] >= '0') && (str[i] <= '9')) || (str[i] == '.')){
+            //printf("DEBUG1: %c\n", str[i]);
+            offset += sprintf(temp + offset, "%c", str[i]);
+        } else {
+            printf("%s\n", temp);
+            return temp;
+        }
+    }
+	return temp;
+}
+
+char *returnHumidity(char *str){
+    static char hum[10] = "";
+    int j = 0;
+    int i;
+    int offset = 0;
+
+    while(str[j] != ','){
+        j++;
+    }
+    j++;
+    for(i = j; str[i] != '\0'; i++){
+
+        if(((str[i] >= '0') && (str[i] <= '9')) || (str[i] == '.')){
+            //printf("DEBUG2: %c\n", str[i]);
+            offset += sprintf(hum + offset, "%c", str[i]);
+        } else {
+            printf("%s\n", hum);
+            return hum;
+        }
+    }
+    printf("%s\n", hum);
+    return hum;
+}
 
 enum mgos_app_init_result mgos_app_init(void) {
   return MGOS_APP_INIT_SUCCESS;
