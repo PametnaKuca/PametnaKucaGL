a6dd2f6dbe976a71815ed0772479c5b7b745c325
Adam Sedmak <adam.sedmak@fer.hr>
2017-10-16 09:30:41 +0200

    Added package parsing

--- build/gen/mg_build_info.c
+++
@@ -1,4 +0,0 @@
-/* Auto-generated, do not edit. */
-const char *mg_build_id = "20171011-120117/???";
-const char *mg_build_timestamp = "2017-10-11T12:01:17Z";
-const char *mg_build_version = "2017101112";
