2b10692a091bd23919adc09ad0e090272ce1e1ab
Adam Sedmak <adam.sedmak@fer.hr>
2017-11-16 13:16:16 +0100

    Minor changes

--- build/gen/mg_build_info.c
+++
@@ -1,4 +0,0 @@
-/* Auto-generated, do not edit. */
-const char *mg_build_id = "20171116-100628/???";
-const char *mg_build_timestamp = "2017-11-16T10:06:28Z";
-const char *mg_build_version = "2017111610";
